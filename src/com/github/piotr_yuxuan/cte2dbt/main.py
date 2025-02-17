bf569d645923ccbfdb646b489cee5d65a139c29c
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2025-02-17 21:48:12 +0000

    Major refactoring
    I shall now use the test resources for a couple of integration tests.

---
+++ src/com/github/piotr_yuxuan/cte2dbt/main.py
@@ -0,0 +1,216 @@
+from typing import Callable, Dict, List, Tuple
+
+from sqlglot import exp
+
+
+def has_table_qualified_name(
+    table: exp.Table,
+) -> bool:
+    return bool(table.db or table.catalog)
+
+
+def is_table_a_cte(
+    cte_names: Dict[str, str],
+    table: exp.Table,
+) -> bool:
+    return not has_table_qualified_name(table) and table.name in cte_names
+
+
+def is_table_a_source(
+    cte_names: Dict[str, str],
+    table: exp.Table,
+) -> bool:
+    # ? Dubious?
+    # return has_table_qualified_name(table) and table.name not in replacements
+    return (has_table_qualified_name(table)) or not (
+        has_table_qualified_name(table) or table.name in cte_names
+    )
+
+
+def cte_table_fn(
+    cte_table: exp.Table,
+    cte_names: Dict[str, str],
+) -> exp.Expression:
+    return exp.Table(
+        this=exp.to_identifier(
+            cte_names[cte_table.name],
+            # Not quoting the name can make the SQL
+            # invalid, but we want to insert raw jinja
+            # template ‑ invalid SQL in themselves.
+            quoted=False,
+        ),
+        alias=exp.to_identifier(
+            cte_table.alias if cte_table.alias else cte_table.name,
+        ),
+    )
+
+
+def transform_cte_tables(
+    cte_expr: exp.Expression,
+    cte_names: Dict[str, str],
+):
+    return cte_expr.copy().transform(
+        lambda node: (
+            cte_table_fn(node, cte_names)
+            if isinstance(node, exp.Table)
+            and is_table_a_cte(
+                cte_names,
+                node,
+            )
+            else node
+        )
+    )
+
+
+def to_fully_qualified_name(table):
+    fully_qualified_name = ".".join(
+        [
+            segment
+            for segment in [
+                table.db,
+                table.catalog,
+                table.name,
+            ]
+            if segment
+        ]
+    )
+    return fully_qualified_name
+
+
+def source_table_fn(
+    source_table: exp.Table,
+    source_names: Dict,
+    to_source_name: Callable,
+) -> exp.Expression:
+    """Beware: because of the walk pattern used, this function uses
+    mutable arguments.
+
+    """
+    fully_qualified_name = to_fully_qualified_name(source_table)
+
+    if fully_qualified_name not in source_names:
+        source_names[fully_qualified_name] = to_source_name(source_table)
+
+    return exp.Table(
+        this=exp.to_identifier(
+            source_names[fully_qualified_name],
+            # Not quoting the name can make the SQL
+            # invalid, but we want to insert raw jinja
+            # template ‑ invalid SQL in themselves.
+            quoted=False,
+        ),
+        alias=exp.to_identifier(
+            source_table.alias if source_table.alias else source_table.name,
+        ),
+    )
+
+
+def transform_source_tables(
+    cte_expr: exp.Expression,
+    cte_names: Dict[str, str],
+    source_names: Dict[str, str],
+    to_source_name: Callable,
+):
+    new_source_names = source_names.copy()
+    new_expr = cte_expr.copy().transform(
+        lambda node: (
+            source_table_fn(node, new_source_names, to_source_name)
+            if isinstance(node, exp.Table)
+            and is_table_a_source(
+                cte_names,
+                node,
+            )
+            else node
+        )
+    )
+    print(f"new_source_names={new_source_names}")
+    return new_expr, new_source_names
+
+
+def get_cte_name_expr_tuples(
+    select: exp.Select,
+) -> List[Tuple[str, exp.Expression]]:
+    if isinstance(select.args.get("with"), exp.With):
+        return [(cte.alias, cte.this) for cte in select.args.get("with")]
+    else:
+        return []
+
+
+def _to_model_expr(
+    cte_names,
+    source_names,
+    cte_expr,
+    to_source_name,
+    model_name,
+    cte_name: str = None,
+):
+    if cte_name:
+        cte_names[cte_name] = model_name
+
+    model_expr = cte_expr
+    model_expr, source_names = transform_source_tables(
+        model_expr,
+        cte_names,
+        source_names,
+        to_source_name,
+    )
+    model_expr = transform_cte_tables(cte_expr, cte_names)
+
+    return model_expr
+
+
+def process_expression(
+    parent_expr: exp.Expression,
+    parent_model_name: str,
+    to_model_name: Callable,
+    to_source_name: Callable,
+    expr_fn: Callable = lambda expr: expr,
+) -> List[Dict]:
+    # I'm not very convinced that this API is currently great. I want
+    # to favour organic growth for now, but at some point we'll need
+    # to check whether we could do better.
+    #
+    # Also, I'm shocked at the argument order in Python, so different
+    # from what we would do in Clojure.
+    final_select_expr: exp.Expression = parent_expr.copy()
+    final_select_expr.args.pop("with")
+    cte_name_and_exprs = get_cte_name_expr_tuples(parent_expr)
+
+    cte_names: Dict[str, str] = dict({})
+    source_names: Dict[str, str] = dict({})
+    models: Dict = dict()
+
+    for cte_name, cte_expr in cte_name_and_exprs:
+        model_name = to_model_name(cte_name)
+        models[model_name] = {
+            "cte_name": cte_name,
+            "cte_expr": expr_fn(cte_expr),
+            "model_expr": expr_fn(
+                _to_model_expr(
+                    cte_names,
+                    source_names,
+                    cte_expr,
+                    to_source_name,
+                    model_name=model_name,
+                    cte_name=cte_name,
+                )
+            ),
+        }
+    models[parent_model_name] = {
+        "cte_expr": expr_fn(cte_expr),
+        "model_expr": expr_fn(
+            _to_model_expr(
+                cte_names,
+                source_names,
+                final_select_expr,
+                to_source_name,
+                model_name=parent_model_name,
+            )
+        ),
+    }
+
+    return {
+        "cte_names": cte_names,
+        "source_names": source_names,
+        "models": models,
+    }
---
+++ tests/com/github/piotr_yuxuan/cte2dbt/main_test.py
@@ -0,0 +1,340 @@
+import importlib
+
+import com.github.piotr_yuxuan.cte2dbt.main as main
+import pytest
+from sqlglot import exp, parse_one
+
+importlib.reload(main)
+
+
+def test_has_table_qualified_name():
+    assert (
+        main.has_table_qualified_name(
+            exp.Table(
+                db="db",
+                catalog="catalog",
+                name="name",
+            )
+        )
+        is True
+    )
+    assert (
+        main.has_table_qualified_name(
+            exp.Table(
+                catalog="catalog",
+                name="name",
+            )
+        )
+        is True
+    )
+    assert (
+        main.has_table_qualified_name(
+            exp.Table(
+                this=exp.to_identifier("name"),
+                alias="alias",
+            ),
+        )
+        is False
+    )
+    assert main.has_table_qualified_name(exp.Table(name="name")) is False
+
+
+@pytest.mark.parametrize(
+    "cte_names, table, expected",
+    [
+        (
+            {"cte1": "stg_cte1"},
+            exp.Table(
+                this=exp.to_identifier("db.catalog.name"),
+            ),
+            False,
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            exp.Table(
+                this=exp.to_identifier("db.catalog.cte1"),
+            ),
+            False,
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            exp.Table(
+                this=exp.to_identifier("catalog.cte1"),
+            ),
+            False,
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            exp.Table(
+                this=exp.to_identifier("cte1"),
+            ),
+            True,
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            exp.Table(
+                this=exp.to_identifier("cte1"),
+                alias="cte1",
+            ),
+            True,
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            exp.Table(
+                this=exp.to_identifier("cte1"),
+                alias="alias",
+            ),
+            True,
+        ),
+    ],
+)
+def test_is_table_a_cte(cte_names, table: exp.Table, expected):
+    assert expected == main.is_table_a_cte(cte_names, table)
+
+
+@pytest.mark.parametrize(
+    "cte_names, table, expected",
+    [
+        (
+            {"cte1": "stg_cte1"},
+            exp.Table(
+                this=exp.to_identifier("db.catalog.name"),
+            ),
+            True,
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            exp.Table(
+                this=exp.to_identifier("db.catalog.cte1"),
+            ),
+            True,
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            exp.Table(
+                this=exp.to_identifier("catalog.cte1"),
+            ),
+            True,
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            exp.Table(
+                this=exp.to_identifier("cte1"),
+            ),
+            False,
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            exp.Table(
+                this=exp.to_identifier("cte1"),
+                alias="cte1",
+            ),
+            False,
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            exp.Table(
+                this=exp.to_identifier("cte1"),
+                alias="alias",
+            ),
+            False,
+        ),
+    ],
+)
+def test_is_table_a_source(cte_names, table: exp.Table, expected):
+    assert expected == main.is_table_a_source(cte_names, table)
+
+
+@pytest.mark.parametrize(
+    "cte_names, query_text, expected",
+    [
+        (
+            {"cte1": "stg_cte1"},
+            "SELECT * FROM cte1",
+            "SELECT * FROM stg_cte1 AS cte1",
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            "SELECT cte1.* FROM cte1",
+            "SELECT cte1.* FROM stg_cte1 AS cte1",
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            "SELECT * FROM cte1 alias",
+            "SELECT * FROM stg_cte1 AS alias",
+        ),
+        (
+            {"my_alias": "stg_my_alias"},
+            "SELECT * FROM cte1 my_alias",
+            "SELECT * FROM cte1 AS my_alias",
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            "SELECT * FROM cte1 JOIN cte1 c on cte1.id = c.id",
+            "SELECT * FROM stg_cte1 AS cte1 JOIN stg_cte1 AS c on cte1.id = c.id",
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            "SELECT * FROM (SELECT * FROM table) cte1",
+            "SELECT * FROM (SELECT * FROM table) cte1",
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            "SELECT * FROM (SELECT * FROM cte1) cte1",
+            "SELECT * FROM (SELECT * FROM stg_cte1 as cte1) cte1",
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            "WITH cte1 as (SELECT * FROM table) SELECT * FROM cte1",
+            "WITH cte1 as (SELECT * FROM table) SELECT * FROM stg_cte1 as cte1",
+        ),
+        (
+            {
+                "cte1": "stg_cte1",
+                "cte2": "stg_cte2",
+            },
+            # Query text:
+            """WITH RECURSIVE
+            cte1 as (SELECT * FROM cte2),
+            cte2 as (SELECT * FROM cte1)
+            SELECT cte1.*, cte2.*
+            FROM cte1
+            JOIN cte2""",
+            # Expected:
+            """WITH RECURSIVE
+            cte1 as (SELECT * FROM stg_cte2 AS cte2),
+            cte2 as (SELECT * FROM stg_cte1 AS cte1)
+            SELECT cte1.*, cte2.*
+            FROM stg_cte1 AS cte1
+            JOIN stg_cte2 AS cte2
+            """,
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            "SELECT * FROM schema.cte1",
+            "SELECT * FROM schema.cte1",
+        ),
+        (
+            {"cte1": "stg_cte1"},
+            "SELECT * FROM db.schema.cte1 cte1",
+            "SELECT * FROM db.schema.cte1 AS cte1",
+        ),
+    ],
+)
+def test_table_name_with_alias_get_replaced(cte_names, query_text, expected):
+    actual = main.transform_cte_tables(parse_one(query_text), cte_names)
+    assert parse_one(expected).sql() == actual.sql()
+
+
+@pytest.mark.parametrize(
+    "query_text, expected_names",
+    [
+        (
+            "SELECT 1",
+            [],
+        ),
+        (
+            "WITH cte1 as (SELECT 1) SELECT 2",
+            ["cte1"],
+        ),
+        (
+            "WITH cte1 as (SELECT 1) WITH cte2 as (SELECT 1) SELECT 2",
+            ["cte1", "cte2"],
+        ),
+        (
+            "WITH cte1 as (WITH cte2 as (SELECT 1) SELECT 2) SELECT 3",
+            ["cte1"],
+        ),
+    ],
+)
+def test_get_name_and_text_for_all_cte(query_text, expected_names):
+    assert expected_names == [
+        name
+        for name, _ in main.get_cte_name_expr_tuples(
+            parse_one(
+                query_text,
+            )
+        )
+    ]
+
+
+@pytest.mark.parametrize(
+    "query_text, expected_cte_expr, expected_source_names",
+    [
+        (
+            "SELECT 1 FROM db.catalog.table",
+            "SELECT 1 FROM {{ source('db') }} AS table",
+            {"catalog.db.table": "{{ source('db') }}"},
+        ),
+        (
+            "SELECT 1 FROM db.catalog.table as table",
+            "SELECT 1 FROM {{ source('db') }} AS table",
+            {"catalog.db.table": "{{ source('db') }}"},
+        ),
+        (
+            "SELECT 1 FROM db.catalog.table as other",
+            "SELECT 1 FROM {{ source('db') }} AS other",
+            {"catalog.db.table": "{{ source('db') }}"},
+        ),
+    ],
+)
+def test_transform_source_tables_implicit_source_names(
+    query_text,
+    expected_cte_expr,
+    expected_source_names,
+):
+    def to_source_name(table: exp.Table) -> str:
+        return "{{ source('" + (table.catalog or table.name) + "') }}"
+
+    actual_cte_expr, actual_source_names = main.transform_source_tables(
+        cte_expr=parse_one(query_text),
+        cte_names=dict(),
+        source_names=dict(),
+        to_source_name=to_source_name,
+    )
+
+    assert expected_cte_expr == actual_cte_expr.sql()
+    assert expected_source_names == actual_source_names
+
+
+@pytest.mark.parametrize(
+    "query_text, source_names, expected_cte_expr, expected_source_names",
+    [
+        (
+            "SELECT 1 FROM db.catalog.table",
+            {},
+            "SELECT 1 FROM {{ source('db') }} AS table",
+            {"catalog.db.table": "{{ source('db') }}"},
+        ),
+        (
+            "SELECT 1 FROM db.catalog.table as table",
+            {"catalog.db.table": "{{ source('source_name', 'table') }}"},
+            "SELECT 1 FROM {{ source('source_name', 'table') }} AS table",
+            {"catalog.db.table": "{{ source('source_name', 'table') }}"},
+        ),
+        (
+            "SELECT 1 FROM db.catalog.table as other",
+            {"catalog.db.table": "{{ source('source_name', 'table') }}"},
+            "SELECT 1 FROM {{ source('source_name', 'table') }} AS other",
+            {"catalog.db.table": "{{ source('source_name', 'table') }}"},
+        ),
+    ],
+)
+def test_transform_source_tables(
+    query_text,
+    source_names,
+    expected_cte_expr,
+    expected_source_names,
+):
+    def to_source_name(table: exp.Table) -> str:
+        return "{{ source('" + (table.catalog or table.name) + "') }}"
+
+    actual_cte_expr, actual_source_names = main.transform_source_tables(
+        cte_expr=parse_one(query_text),
+        cte_names=dict(),
+        source_names=source_names,
+        to_source_name=to_source_name,
+    )
+
+    assert expected_cte_expr == actual_cte_expr.sql()
+    assert expected_source_names == actual_source_names
